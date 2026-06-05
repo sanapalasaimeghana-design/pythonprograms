e75ff7845a3a44176ce4d3621451f1fb67fd3869
sanapalasaimeghana-design <sanapalasaimeghana@gmail.com>
2026-06-05 16:22:58 +0530

    program3.py is added

---
+++ program17.py
@@ -0,0 +1,8 @@
+for i in range(10):
+	print(i)
+#================
+print()
+#================
+n=int(input("enter a no:"))
+for i in range(n):
+	print(i)
---
+++ program24.py
@@ -0,0 +1,3 @@
+n=int(input("enter n value"))
+for i in range(n+1):
+	print(n,"x",i,"=",n*i)
